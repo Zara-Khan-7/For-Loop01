5f09188ec052ef5d246c0b37e7cec051c213a94a
Zara-Khan-7 <zaara.khan1795.zy@gmail.com>
2024-05-30 20:23:47 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+// Creating an Array
+let myFavFood = ["Pasta", "Pizza", "Burger", "Chowmein"];
+// Print by using For-Loop
+for (let favFood_one of myFavFood) {
+    console.log(`\tI Like ${favFood_one} very much.\n`);
+}
+// Printing the message outside the For-Loop
+console.log("\tThese are my Fav Foods.");
---
+++ main.ts
@@ -0,0 +1,10 @@
+// Creating an Array
+let myFavFood = ["Pasta","Pizza","Burger","Chowmein"]
+
+// Print by using For-Loop
+ for(let favFood_one of myFavFood){
+    console.log(`\tI Like ${favFood_one} very much.\n`)
+ }
+
+// Printing the message outside the For-Loop
+console.log("\tThese are my Fav Foods.")
